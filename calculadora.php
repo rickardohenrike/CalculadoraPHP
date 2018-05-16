d6911ba0092cc7c40ba99a6b8462ab2e076d95bf
Ricardo <ricardohenrique_sg@hotmail.com>
2018-05-16 00:47:11 -0300

    Primeiro commit, aprendendo PHP

---
+++ calculadora.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+  
+    <title>Calculadora</title>
+   <link rel="stylesheet" href="_css/estilo.css"
+</head>
+<body>
+    <div>
+<?php 
+$n1 = $_GET["x"];
+$n2 = $_GET["y"];
+
+echo "A soma dos dois números foi ". ( $n1+$n2);
+
+
+?>
+
+    </div>
+
+</body>
+</html>
